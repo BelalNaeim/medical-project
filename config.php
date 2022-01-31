48a4efef3cd4d072a3a67be4ea32147719589db8
BelalNaeim <belal.naeem@yahoo.com>
2022-01-31 18:54:14 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+// base file link
+define("BL", __DIR__.'/');
+define("BLA", __DIR__.'/admin/');
+
+// base url
+define("BU", "http://medical_serv.devel/");
+define("BUA", "http://medical_serv.devel/admin/");
+define("ASSETS", "http://medical_serv.devel/assets/");
+
+require BL.'functions/db.php';
+
+
+
+
+?>
+
